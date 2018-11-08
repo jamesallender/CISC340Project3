5174b6066b4fd73932a6b3183d8e8ac3ae8367d9
James <jamesa52416@gmail.com>
2018-11-08 15:53:31 -0600

    starter

--- project3_alle30842_li000053/pipelineSimulator.c
+++ project3_alle30842_li000053/pipelineSimulator.c
@@ -59,6 +59,50 @@ typedef struct statestruct{
 } statetype;
 
 int main(int argc, char** argv){
+
+	/** Get command line arguments **/
+    char* fname;
+
+	opterr = 0;
+
+	int cin = 0;
+
+	while((cin = getopt(argc, argv, "i:")) != -1){
+		switch(cin)
+		{
+			case 'i':
+				fname=(char*)malloc(strlen(optarg));
+				fname[0] = '\0';
+
+				strncpy(fname, optarg, strlen(optarg)+1);
+				printf("FILE: %s\n", fname);
+				break;
+			case '?':
+				if(optopt == 'i'){
+					printf("Option -%c requires an argument.\n", optopt);
+				}
+				else if(isprint(optopt)){
+					printf("Unknown option `-%c'.\n", optopt);
+				}
+				else{
+					printf("Unknown option character `\\x%x'.\n", optopt);
+					return 1;
+				}
+				break;
+			default:
+				abort();
+		}
+	}
+
+	FILE *fp = fopen(fname, "r");
+	if (fp == NULL) {
+		printf("Cannot open file '%s' : %s\n", fname, strerror(errno));
+		return -1;
+	}
+	// reset fp to the beginning of the file
+	rewind(fp);
+
+
 	while(1){
 		printstate(&state);
 		/* check for halt */
@@ -98,4 +142,90 @@ int main(int argc, char** argv){
 							state with the values calculated in this cycle
 							– AKA “Clock Tick”. */
 	}
+}
+
+int field0(int instruction){
+	return( (instruction>>19) & 0x7);
+}
+int field1(int instruction){
+	return( (instruction>>16) & 0x7);
+}
+int field2(int instruction){
+	return(instruction & 0xFFFF);
+}
+int opcode(int instruction){
+	return(instruction>>22);
+}
+
+void printinstruction(int instr) {
+	char opcodestring[10];
+	if (opcode(instr) == ADD) {
+		strcpy(opcodestring, "add");
+	} else if (opcode(instr) == NAND) {
+		strcpy(opcodestring, "nand");
+	} else if (opcode(instr) == LW) {
+		strcpy(opcodestring, "lw");
+	} else if (opcode(instr) == SW) {
+		strcpy(opcodestring, "sw");
+	} else if (opcode(instr) == BEQ) {
+		strcpy(opcodestring, "beq");
+	} else if (opcode(instr) == JALR) {
+		strcpy(opcodestring, "jalr");
+	} else if (opcode(instr) == HALT) {
+		strcpy(opcodestring, "halt");
+	} else if (opcode(instr) == NOOP) {
+		strcpy(opcodestring, "noop");
+	} else {
+		strcpy(opcodestring, "data");
+	}
+	if(opcode(instr) == ADD || opcode(instr) == NAND){
+		printf("%s %d %d %d\n", opcodestring, field2(instr), field0(instr), field1(instr));
+	}else if(0 == strcmp(opcodestring, "data")){
+		printf("%s %d\n", opcodestring, signextend(field2(instr)));
+	}else{
+		printf("%s %d %d %d\n", opcodestring, field0(instr), field1(instr),
+		signextend(field2(instr)));
+	}
+}
+
+void printstate(statetype *stateptr){
+	int i;
+	printf("\n@@@\nstate before cycle %d starts\n", stateptr->cycles);
+	printf("\tpc %d\n", stateptr->pc);
+
+	printf("\tdata memory:\n");
+		for (i=0; i<stateptr->nummemory; i++) {
+			printf("\t\tdatamem[ %d ] %d\n", i, stateptr->datamem[i]);
+		}
+
+	printf("\tregisters:\n");
+		for (i=0; i<NUMREGS; i++) {
+			printf("\t\treg[ %d ] %d\n", i, stateptr->reg[i]);
+		}
+
+	printf("\tIFID:\n");
+		printf("\t\tinstruction ");
+		printinstruction(stateptr->IFID.instr);
+		printf("\t\tpcplus1 %d\n", stateptr->IFID.pcplus1);
+	printf("\tIDEX:\n");
+		printf("\t\tinstruction ");
+		printinstruction(stateptr->IDEX.instr);
+		printf("\t\tpcplus1 %d\n", stateptr->IDEX.pcplus1);
+		printf("\t\treadregA %d\n", stateptr->IDEX.readregA);
+		printf("\t\treadregB %d\n", stateptr->IDEX.readregB);
+		printf("\t\toffset %d\n", stateptr->IDEX.offset);
+	printf("\tEXMEM:\n");
+		printf("\t\tinstruction ");
+		printinstruction(stateptr->EXMEM.instr);
+		printf("\t\tbranchtarget %d\n", stateptr->EXMEM.branchtarget);
+		printf("\t\taluresult %d\n", stateptr->EXMEM.aluresult);
+		printf("\t\treadreg %d\n", stateptr->EXMEM.readreg);
+	printf("\tMEMWB:\n");
+		printf("\t\tinstruction ");
+		printinstruction(stateptr->MEMWB.instr);
+		printf("\t\twritedata %d\n", stateptr->MEMWB.writedata);
+	printf("\tWBEND:\n");
+		printf("\t\tinstruction ");
+		printinstruction(stateptr->WBEND.instr);
+		printf("\t\twritedata %d\n", stateptr->WBEND.writedata);
 }
